82436fcc32a32d8df5eceb17605627c69a59e9e8
Рябов Григорий Андреевич <GRyabov@alfabank.ru>
2021-03-17 16:14:00 +0400

    Initial commit

---
+++ src/main/java/ru/spring/auto/rest/docs/demo/SpringAutoRestDocsDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.spring.auto.rest.docs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoRestDocsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutoRestDocsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spring/auto/rest/docs/springautorestdocsdemo/SpringAutoRestDocsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.auto.rest.docs.springautorestdocsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoRestDocsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
